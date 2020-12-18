409527247e2ec2da6efcf8c4468ef12ca2119adb
iamprady <iamprady99@gmail.com>
2020-12-19 01:08:14 +0530

    pwa tweaks

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,28 @@ importScripts(
   'https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/5.1.4/workbox-sw.min.js'
 )
 workbox.routing.registerRoute(
+  ({ url }) =>
+    url.origin === 'https://fonts.googleapis.com' ||
+    url.origin === 'https://fonts.gstatic.com',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts',
+    plugins: [new workbox.expiration.ExpirationPlugin({ maxEntries: 20 })],
+  })
+)
+workbox.routing.registerRoute(
+  ({ request }) =>
+    request.destination === 'script' || request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate()
+)
+workbox.routing.registerRoute(
   ({ request }) => request.destination === 'image',
-  new workbox.strategies.NetworkFirst()
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
 )
